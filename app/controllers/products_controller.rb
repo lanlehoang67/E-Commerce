d8027d0485829accce28bb13e9912f05f5a5fb57
lanlehoang10 <lanlehoang10@gmail.com>
2019-05-12 13:28:45 +0700

    deo biet lam sao

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,15 @@
 class ProductsController < ApplicationController
   before_action :load_product, only: :show
+  before_action :load_products, only: :show
   def show
   end
 
   private
+
+  def load_products
+    @top_products = Product.best_seller
+    @special_products = Product.special
+  end
 
   def load_product
     @product = Product.find_by id: params[:id]
